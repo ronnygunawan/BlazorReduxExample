311bb7d3a80e61bb2f7b06e5d8dac1c7b58cbeb1
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-12-10 15:44:10 +0700

    Add sample code

---
+++ BlazorReduxExample/Stores/CurrentCountStore.cs
@@ -0,0 +1,22 @@
+﻿using RG.Redux;
+
+namespace BlazorReduxExample.Stores;
+
+public interface ICurrentCountEvent : IEvent { }
+public record CurrentCountIncremented : ICurrentCountEvent;
+
+public record CurrentCountStore() : Store<int, ICurrentCountEvent>(
+    reducer: (state, action) => action switch
+    {
+        CurrentCountIncremented => state + 1,
+        _ => state
+    },
+    initialValue: 0
+)
+{
+    private static readonly CurrentCountIncremented _incremented = new();
+    
+    public void Increment() => Dispatch(_incremented);
+}
+
+public record SingletonCurrentCountStore : CurrentCountStore;
---
+++ README.md
@@ -0,0 +1,3 @@
+# Blazor + RG.Redux Example
+
+This is a simple example of how to use [RG.Redux](https://github.com/ronnygunawan/RG.Redux) in a Blazor application.
